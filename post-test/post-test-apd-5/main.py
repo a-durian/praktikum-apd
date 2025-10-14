77d35f69470fd2f1053e17d3b82f708c42649b82
a-durian <muhammadadrian7333@gmail.com>
2025-10-14 15:17:29 +0800

    prosessss

--- post-test/post-test-apd-5/main.py
+++ post-test/post-test-apd-5/main.py
@@ -18,30 +18,46 @@ while pilihan_karakter != '1' or pilihan_karakter != '2':
         print('3. ')
         break
     elif pilihan_karakter == '2':
-        print("====SELAMAT DATANG DI CODEMARET===")
-        print("Anda belum punya akun, silahkan sign up terlebih dahulu.")
+        print("="*61)
+        print("============== Selamat Datang di KLikCodemaret ==============")
+        print("="*61)
+        print("Anda belum mempunyai akun, silahkan sign up terlebih dahulu.")
         nama_customer = input("Masukkan Nama: ")
         pw_customer = input("Masukkan Password: ")
-        print("Sign up berhasil! silahkan login dengan akun anda:")
-        login_NC = input("Nama: ")
-        login_PC = input("Password: ")
+        print("\nSign up berhasil! silahkan login dengan akun anda:")
         
-        if login_NC == nama_customer and  login_PC == pw_customer:
-            print(f"===Login berhasil! Selamat datang di KlikCodemaret===")
-            print("="*52)
-            print("1. Grosir & Makanan")
-            print("2. Keranjang Belanja")
-            print("3. Transaksi")
-            opsi_customer = input("Pilih opsi yang tersedia diatas [1/2/3]: ")
-            if opsi_customer == '1':
-                print("===Grosir & Makanan===")
-                print()#LIST PRODUK-PRODUK
-            elif opsi_customer == '2':
-                print("===Keranjang Belanja===")
-                print()#LIST KERANJANG BELANJA(Berikan output 'kosong' bila belum ada produk didalam keranjang)
-            elif opsi_customer == '3':
-                print("===Transaksi===")
+        percobaan = 5
+        while percobaan > 0:
+            login_NC = input("Nama: ")
+            login_PC = input("Password: ")
+        
+            if login_NC == nama_customer and  login_PC == pw_customer:
+                print("="*57)
+                print(f"=====Login berhasil! Selamat datang di KlikCodemaret=====")
+                print("="*57)
+                print("1. Grosir")
+                print("2. Keranjang Belanja")
+                print("3. Transaksi")
+                opsi_customer = input("Pilih opsi yang tersedia diatas [1/2/3]: ")
+                if opsi_customer == '1':
+                    print("===Grosir===")
+                    print()#LIST PRODUK-PRODUK
+                    input("Pilih Kategori Belanja [1/2/3/4]: ")
+                elif opsi_customer == '2':
+                    print("===Keranjang Belanja===")
+                    print()#LIST KERANJANG BELANJA(Berikan output 'kosong' bila belum ada produk didalam keranjang)
+                elif opsi_customer == '3':
+                    print("===Transaksi===")
+                break
+            elif login_NC != nama_customer or login_PC != pw_customer:
+                percobaan -= 1
+                if percobaan == 0:
+                    print("Login gagal! Program dihentikan.")
+                    break
+                else:
+                    print("\nNama atau Password yang anda masukkan salah! Silahkan coba lagi.")
+                    print(f"Tersisa {percobaan}x Percobaaan.\n")
         break
     else:
         print("\nTolong ikuti instruksi yang tersedia. Coba lagi.\n")
-print("Tembus!")
+print("\nTembus kebawah!")
